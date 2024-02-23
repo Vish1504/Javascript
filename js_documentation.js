a89f235ff070c95e32d82bb60afc61a1f49823cb
Vishnu Nair <visair1504@gmail.com>
2024-02-24 01:52:40 +0530

    Added comparison operators, control statements, arrays , array methods

--- js_documentation.js
+++ js_documentation.js
@@ -1,3 +1,5 @@
+const log = require("node-forge/lib/log");
+
 // Display a popup box with the message "Hello"
 alert("Hello");
 
@@ -118,6 +120,102 @@ function square(number) {
 console.log(square(7)) //Output: Square of 7 = 49
 
 // Note: If a function does not explicitly return a value, it returns undefined by default. 
+
+// Comparison operators :
+// 1) == -> It checks only for the value but not the type
+5 == 5 //true
+5 == '5' //true
+5 == 8 //false
+5 == '1' //false
+// 2) === -> It checks for the value AND the type
+5 === 5 //true
+5 === '5' //false
+
+
+// Control Statements
+/*
+    1) If-else:
+
+        if (condition is true){
+            Do this
+        }
+        else{
+            Do this
+        }
+    
+    2) while loop: It allows you to repeatedly execute a block of code as long as a specified condition evaluates to true. 
+
+    while(condition is TRUE){
+        // Execute this
+    }
+
+    3) for loop: It allows you to repeatedly execute the code block as long as the condition is set to true the condition for a specific number of times. It is similar to a while loop, but in a for loop you'd have an idea of the number of iterations needed.
+
+            for (initialization; condition; update) {
+            // Code block to be executed repeatedly
+        }
+
+*/
+
+function learn_if_else(num) {
+    if (num > 5) {
+        console.log("Condition PASSED");
+    }
+    else {
+        console.log("Condition FAILED");
+    }
+}
+
+learn_if_else(7) //Condition PASSED
+learn_if_else(2) //condition FAILED
+
+// Arrays: It is an object which stores a collection of items under the same variable name. They can have elements of different data types, and they can dynamically adjust in size.
+let names = ["harry", "Percy", "Dwight", "Naruto"]
+console.log(names[2]); //Dwight
+
+// Length of an array
+console.log(names.length); //4
+
+// Few common Array methods:
+// 1) concat() : It is used to merge two or more arrays byt resulting in a new merged array.
+let arr1 = ['a', 'b', 'c'];
+let arr2 = ['d', 'e', 'f'];
+let arr3 = arr1.concat(arr2);
+console.log(arr3); // Output: [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+
+// 2)reverse() : It reverses an array inplace and ends up changing the original array as well
+let arr4 = ['a', 'b', 'c'];
+let rev_arr4 = arr4.reverse();
+console.log("arr4 " + arr4); //arr4 c,b,a
+console.log("reversed arr4 " + rev_arr4); //reversed arr4 c,b,a
+console.log("arr4 " + arr4); //arr4 c,b,a
+
+// 3)push(num) : It helps to add the element "num" to the end of the array
+console.log(arr4); // [ 'c', 'b', 'a' ]
+arr4.push("Snoop Dogg");
+console.log(arr4) //[ 'c', 'b', 'a', 'Snoop Dogg' ]
+
+// 4)pop(): It helps to REMOVE the LAST element in the array and then return that element.
+console.log(arr4); //[ 'c', 'b', 'a', 'Snoop Dogg' ]
+console.log(arr4.pop()); //Snoop Dogg
+console.log(arr4); //[ 'c', 'b', 'a' ]
+
+// 5)shift(): It helps to REMOVE the FIRST element to the array and then return that element.
+console.log(arr4); //[ 'c', 'b', 'a' ]
+console.log(arr4.shift()); //c
+console.log(arr4); //[ 'b', 'a' ]
+
+// 6)unshift(): It ADDS the specified element to the BEGINNING of the array
+console.log(arr4); //[ 'b', 'a' ]
+arr4.unshift("Snoop is back");
+console.log(arr4); // [ 'Snoop is back', 'b', 'a' ]
+
+
+
+
+
+
+
 
 
 
